b123716e556037d1260cc043ac94b0596dfbe2c0
Pedro <pedropiovesan2002@gmail.com>
2025-04-28 00:53:15 -0300

    Contador de clique

---
+++ app/src/main/java/com/example/contador_de_clique/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.contador_de_clique;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int quantidade = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void contador (View view){
+        TextView txtContador = findViewById(R.id.txtContador);
+        quantidade++;
+        txtContador.setText("Contador: " + quantidade);
+    }
+}
