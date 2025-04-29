ea5784bed5dbba8d67c5007501b4d030c4dbc45b
Pedro <pedropiovesan2002@gmail.com>
2025-04-28 23:44:41 -0300

    Sorteio correto

---
+++ Sorteio_certo/app/src/main/java/com/example/sorteio_certo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sorteio_certo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void SortearNumero (View view){
+        EditText num1 = findViewById(R.id.num1);
+        EditText num2 = findViewById(R.id.num2);
+        String num1Digitado = num1.getText().toString();
+        String num2Digitado = num2.getText().toString();
+
+        int min = Integer.parseInt(num1Digitado);
+        int max = Integer.parseInt(num2Digitado);
+
+        TextView textResultado = findViewById(R.id.textResultado);
+
+        int sorteio = new Random().nextInt(min,max);
+        textResultado.setText("O número sorteado foi: " + sorteio);
+    }
+
+}
