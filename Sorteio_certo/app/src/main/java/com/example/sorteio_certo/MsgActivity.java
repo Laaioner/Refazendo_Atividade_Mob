718046cefd6b334fe2066b3249cfb641942a5e6d
Pedro <pedropiovesan2002@gmail.com>
2025-05-04 15:43:50 -0300

    Código final IMC _ COM AS IMAGENS

--- Sorteio_certo/app/src/main/java/com/example/sorteio_certo/MsgActivity.java
+++ Sorteio_certo/app/src/main/java/com/example/sorteio_certo/MsgActivity.java
@@ -1,6 +1,7 @@
 package com.example.sorteio_certo;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -17,27 +18,42 @@ public class MsgActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
 
+        ImageView imgIMC = findViewById(R.id.imgIMC);
+        TextView txtIMC = findViewById(R.id.txtIMC);
         TextView txtResultado = findViewById(R.id.txtResultado);
         Bundle bundle = getIntent().getExtras();
 
-
-
         String imcRecebe = getIntent().getStringExtra("imc");
         float calcImc = Float.parseFloat(imcRecebe);
-
         if (calcImc < 18.5) {
-            txtResultado.setText(bundle.getString("imc", "Abaixo do peso"));
-        } else if (calcImc < 25.0) {
-            txtResultado.setText(bundle.getString("imc", "Abaixo do peso"));
-        } else if (calcImc < 30.0) {
-            txtResultado.setText(bundle.getString("imc", "Abaixo do peso"));
+            txtResultado.setText(imcRecebe);
+            txtIMC.setText("Abaixo do peso");
+            imgIMC.setImageResource(R.drawable.baixo_peso);
 
-        } else if (calcImc < 35.0) {
-            txtResultado.setText(bundle.getString("imc", "Abaixo do peso"));
-        } else if (calcImc < 40.0) {
-            txtResultado.setText(bundle.getString("imc", "Abaixo do peso"));
+        } else if (calcImc < 25) {
+            txtResultado.setText(imcRecebe);
+            txtIMC.setText("Peso normal");
+            imgIMC.setImageResource(R.drawable.peso_normal);
+
+        } else if (calcImc < 30) {
+            txtResultado.setText(imcRecebe);
+            txtIMC.setText("Sobrepeso");
+            imgIMC.setImageResource(R.drawable.excesso_de_peso);
+
+        } else if (calcImc < 35) {
+            txtResultado.setText(imcRecebe);
+            txtIMC.setText("Obesidade grau I");
+            imgIMC.setImageResource(R.drawable.obesidade);
+
+        } else if (calcImc < 40) {
+            txtResultado.setText(imcRecebe);
+            txtIMC.setText("Obesidade grau II");
+            imgIMC.setImageResource(R.drawable.obesidade);
+
         } else {
-            txtResultado.setText(bundle.getString("imc", "Abaixo do peso"));
+            txtResultado.setText(imcRecebe);
+            txtIMC.setText("Obesidade grau III");
+            imgIMC.setImageResource(R.drawable.obesidade_extrema);
         }
     }
 }
